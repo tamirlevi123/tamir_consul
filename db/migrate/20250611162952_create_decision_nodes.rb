bcc63e68d189b2ac7af6e2b89229234247926243
Tamir Levy <tamirlevi123@gmail.com>
2025-06-11 20:59:50 +0300

    Fix: Update decision_nodes migration to use self-referential foreign key

--- db/migrate/20250611162952_create_decision_nodes.rb
+++ db/migrate/20250611162952_create_decision_nodes.rb
@@ -2,7 +2,7 @@ class CreateDecisionNodes < ActiveRecord::Migration[7.1]
   def change
     create_table :decision_nodes do |t|
       t.text :content
-      t.references :parent, foreign_key: true
+      t.references :parent, foreign_key: { to_table: :decision_nodes }
       t.references :decision_tree, foreign_key: true
 
       t.timestamps
