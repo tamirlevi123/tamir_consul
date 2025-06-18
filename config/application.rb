ae474c686bcecc6ff317a91eb932e2596cf55e58
Tamir Levy <tamirlevi123@gmail.com>
2025-06-18 16:23:06 +0300

    Added Hebrew translations and RTL support for decision tree modal

--- config/application.rb
+++ config/application.rb
@@ -70,60 +70,10 @@ module Consul
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "*", "*.{rb,yml}")]
     config.i18n.default_locale = :he
-    available_locales = [
-      "ar",
-      "bg",
-      "bs",
-      "ca",
-      "cs",
-      "da",
-      "de",
-      "el",
-      "en",
-      "es",
-      "es-PE",
-      "eu",
-      "fa",
-      "fr",
-      "gd",
-      "gl",
-      "he",
-      "hr",
-      "id",
-      "it",
-      "ka",
-      "ne",
-      "nl",
-      "oc",
-      "pl",
-      "pt",
-      "pt-BR",
-      "ro",
-      "ru",
-      "sl",
-      "sq",
-      "so",
-      "sr",
-      "sv",
-      "tr",
-      "uk-UA",
-      "val",
-      "zh-CN",
-      "zh-TW"
-    ]
-    config.i18n.available_locales = available_locales
-    config.i18n.fallbacks = [I18n.default_locale, {
-      "ca" => "es",
-      "es-PE" => "es",
-      "eu" => "es",
-      "fr" => "es",
-      "gl" => "es",
-      "it" => "es",
-      "oc" => "fr",
-      "pt-BR" => "es",
-      "val" => "es"
-    }]
+    config.i18n.available_locales = [:he, :en, :es]
+    config.i18n.fallbacks = [:he, :en]
 
     initializer :exclude_custom_locales_automatic_loading, before: :add_locales do
       paths.add "config/locales", glob: "**[^custom]*/*.{rb,yml}"
