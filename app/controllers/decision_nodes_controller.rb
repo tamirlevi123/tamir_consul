b2567289ba6917c099417b080dcdc765f6af983b
Tamir Levy <tamirlevi123@gmail.com>
2025-06-18 18:45:51 +0300

    Allow any logged-in user to add nodes to decision trees - removed authorization restriction

--- app/controllers/decision_nodes_controller.rb
+++ app/controllers/decision_nodes_controller.rb
@@ -2,7 +2,6 @@ class DecisionNodesController < ApplicationController
   skip_authorization_check
   before_action :authenticate_user!
   before_action :set_decision_tree
-  before_action :authorize_user!
 
   def new
     @decision_node = @decision_tree.decision_nodes.new
@@ -27,11 +26,5 @@ class DecisionNodesController < ApplicationController
 
   def decision_node_params
     params.require(:decision_node).permit(:content, :url, :parent_id, :url_confirmed)
-  end
-
-  def authorize_user!
-    unless @decision_tree.user == current_user
-      redirect_to decision_trees_path, alert: t("shared.not_authorized")
-    end
   end
 end 
