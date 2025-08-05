46805b7e387e1d3640d1c87ce1be09b6c5ac2346
Tamir Levy <tamirlevi123@gmail.com>
2025-08-05 12:01:00 +0300

    Translations 101

--- app/lib/remote_translations/microsoft/client.rb
+++ app/lib/remote_translations/microsoft/client.rb
@@ -2,6 +2,7 @@ class RemoteTranslations::Microsoft::Client
   include RemoteTranslations::Microsoft::SentencesParser
   CHARACTERS_LIMIT_PER_REQUEST = 5000
   PREVENTING_TRANSLATION_KEY = "notranslate".freeze
+  API_ENDPOINT = "https://api.cognitive.microsofttranslator.com/translate".freeze
 
   def call(fields_values, locale)
     texts = prepare_texts(fields_values)
@@ -21,16 +22,12 @@ class RemoteTranslations::Microsoft::Client
 
   private
 
-    def client
-      @client ||= BingTranslator.new(Tenant.current_secrets.microsoft_api_key)
-    end
-
     def request_translation(texts, locale)
       response = []
       split_response = false
 
       if characters_count(texts) <= CHARACTERS_LIMIT_PER_REQUEST
-        response = client.translate_array(texts, to: locale)
+        response = translate_texts(texts, locale)
       else
         texts.each do |text|
           response << translate_text(text, locale)
@@ -43,8 +40,45 @@ class RemoteTranslations::Microsoft::Client
 
     def translate_text(text, locale)
       fragments_for(text).map do |fragment|
-        client.translate_array([fragment], to: locale)
+        translate_texts([fragment], locale)
       end.flatten
+    end
+
+    def translate_texts(texts, locale)
+      uri = URI(API_ENDPOINT)
+      uri.query = URI.encode_www_form({
+        'api-version' => '3.0',
+        'to' => locale
+      })
+
+      request = Net::HTTP::Post.new(uri)
+      request['Ocp-Apim-Subscription-Key'] = Tenant.current_secrets.microsoft_api_key
+      request['Ocp-Apim-Subscription-Region'] = 'westeurope'  # Add region header
+      request['Content-Type'] = 'application/json'
+      request['X-ClientTraceId'] = SecureRandom.uuid
+
+      # Format texts for Azure Translator API
+      body = texts.map { |text| { 'text' => text } }.to_json
+      request.body = body
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+      response = http.request(request)
+
+      if response.code == '200'
+        parse_azure_response(response.body)
+      else
+        raise "Translation failed: #{response.code} - #{response.body}"
+      end
+    end
+
+    def parse_azure_response(response_body)
+      json_response = JSON.parse(response_body)
+      json_response.map { |item| item['translations'].first['text'] }
+    rescue JSON::ParserError => e
+      raise "Failed to parse Azure response: #{e.message}"
     end
 
     def parse_response(response, split_response)
